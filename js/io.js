95533b3a1907f8da3d1a8f0ec5b183c1b5854b6e
Eduardo Vernier <efvernier@gmail.com>
2014-11-03 23:58:26 -0200

    PowerUps!

--- js/io.js
+++ js/io.js
@@ -46,3 +46,39 @@ function mouseMonitor(event){
 function float2int (value) {
 	return value | 0;
 };
+
+document.getElementById('fez').addEventListener('click', function (e) {
+	if (totalMoney > 400){
+		YellowTower.cooldown = YellowTower.cooldown*0.80; 
+		GreenTower.cooldown = GreenTower.cooldown*0.80; 
+		BlueTower.cooldown = BlueTower.cooldown*0.80; 
+		OrangeTower.cooldown = OrangeTower.cooldown*0.80; 
+		totalMoney-=400;
+		var elem = document.getElementById('fezBlock');
+		elem.parentNode.removeChild(elem);
+	}
+});
+
+document.getElementById('g3d').addEventListener('click', function (e) {
+	if (totalMoney > 500){
+		YellowTower.radius = YellowTower.radius*13; 
+		GreenTower.radius = GreenTower.radius*1.3; 
+		BlueTower.radius = BlueTower.radius*1.3; 
+		OrangeTower.radius = OrangeTower.radius*1.3; 
+		totalMoney-=400;
+		var elem = document.getElementById('g3dBlock');
+		elem.parentNode.removeChild(elem);
+	}
+});
+
+document.getElementById('bowTie').addEventListener('click', function (e) {
+	if (totalMoney > 600){
+		YellowTower.impact = YellowTower.impact*1.3; 
+		GreenTower.impact = GreenTower.impact*1.3; 
+		BlueTower.impact = BlueTower.impact*1.3; 
+		OrangeTower.impact = OrangeTower.impact*1.3; 
+		totalMoney-=400;
+		var elem = document.getElementById('bowTieBlock');
+		elem.parentNode.removeChild(elem);
+	}
+});
